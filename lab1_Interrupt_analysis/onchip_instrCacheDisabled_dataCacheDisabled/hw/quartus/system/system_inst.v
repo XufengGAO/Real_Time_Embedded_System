e3f88d581570561ae3ded286c2e204f903b556ff
XufengGAO <2483307804@qq.com>
2022-06-03 22:30:01 +0200

    updates

--- lab1_Interrupt_analysis/onchip_instrCacheDisabled_dataCacheDisabled/hw/quartus/system/system_inst.v
+++
@@ -1,6 +0,0 @@
-	system u0 (
-		.clk_clk                         (<connected-to-clk_clk>),                         //                     clk.clk
-		.custompio_0_conduit_end_parport (<connected-to-custompio_0_conduit_end_parport>), // custompio_0_conduit_end.parport
-		.reset_reset_n                   (<connected-to-reset_reset_n>)                    //                   reset.reset_n
-	);
-
